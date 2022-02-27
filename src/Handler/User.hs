c0d92af0a9f678fd418bcd7ba2e15dcfa452a5e9
GabrielDertoni <gab.dertoni@gmail.com>
2022-02-27 00:30:34 -0300

    Safer user post endpoint
    Now the user post endpoint won't allow users to set their own balance or
    their own register date. Those values will be set automatically. Every
    newly crated user starts with a balance of 0 Stellarium Coins.

--- src/Handler/User.hs
+++ src/Handler/User.hs
@@ -5,27 +5,43 @@ import Data.Aeson.Types
 import Data.Aeson (toJSON)
 import Data.Maybe
 import Data.Text
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Data.Time.Calendar (toGregorian)
 import Database.Persist.Postgresql
 import GHC.Generics
 import Network.HTTP.Types
 
-import Database.Models
+import qualified Database.Models as DB
 import Foundation
+
+data UserCreate = UserCreate { name :: Text
+                             , username :: Text
+                             , birthdate :: DB.Date
+                             } deriving (Generic, FromJSON)
 
 postUserR :: Handler Value
 postUserR = do
-    user <- requireCheckJsonBody :: Handler User
+    userCrate <- requireCheckJsonBody :: Handler UserCreate
+    now <- liftIO getCurrentTime
+    let (year, month, day) = toGregorian $ utctDay now
+    let user = DB.User { DB.userName = name userCrate
+                       , DB.userUsername = username userCrate
+                       , DB.userBirthdate = birthdate userCrate
+                       , DB.userDate = DB.Date { DB.year = fromIntegral year
+                                               , DB.month = fromIntegral month
+                                               , DB.day = fromIntegral day
+                                               }
+                       , DB.userBalance = DB.Stellarium 0
+                       }
     userId <- runDB $ insert user
 
     sendStatusJSON created201
-        $ object ["result" .= fromSqlKey userId]
+        $ object ["userId" .= fromSqlKey userId]
 
-
-getUserByIdR :: UserId -> Handler Value
+getUserByIdR :: DB.UserId -> Handler Value
 getUserByIdR userId = do
     user <- runDB $ get404 userId
     sendStatusJSON ok200 $ toJSON user
-
 
 {-
 data UserPatch = UserPatch
@@ -52,7 +68,7 @@ patchUserByIdR userId = do
     sendStatusJSON noContent204 emptyObject
 -}
 
-deleteUserByIdR :: UserId -> Handler Value
+deleteUserByIdR :: DB.UserId -> Handler Value
 deleteUserByIdR userId = do
     runDB $ delete userId
     sendStatusJSON noContent204 emptyObject
