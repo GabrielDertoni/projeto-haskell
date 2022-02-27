c5ebc142ca036e96ad10626c865f17f3cd8a2fdc
GabrielDertoni <gab.dertoni@gmail.com>
2022-02-27 00:43:03 -0300

    Add `email` column for users + update users

--- src/Handler/User.hs
+++ src/Handler/User.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Data.Text
 import Data.Time.Clock (getCurrentTime, utctDay)
 import Data.Time.Calendar (toGregorian)
+import Data.Maybe (catMaybes)
 import Database.Persist.Postgresql
 import GHC.Generics
 import Network.HTTP.Types
@@ -14,24 +15,26 @@ import Network.HTTP.Types
 import qualified Database.Models as DB
 import Foundation
 
-data UserCreate = UserCreate { name :: Text
-                             , nickname :: Text
-                             , birthdate :: DB.Date
+data UserCreate = UserCreate { createUserName :: Text
+                             , createUserNickname :: Text
+                             , createUserEmail :: Text
+                             , createUserBirthdate :: DB.Date
                              } deriving (Generic, FromJSON)
 
 postUserR :: Handler Value
 postUserR = do
-    userCrate <- requireCheckJsonBody :: Handler UserCreate
+    UserCreate{..} <- requireCheckJsonBody :: Handler UserCreate
     now <- liftIO getCurrentTime
     let (year, month, day) = toGregorian $ utctDay now
-    let user = DB.User { DB.userName = name userCrate
-                       , DB.userNickname = nickname userCrate
-                       , DB.userBirthdate = birthdate userCrate
-                       , DB.userDate = DB.Date { DB.year = fromIntegral year
-                                               , DB.month = fromIntegral month
-                                               , DB.day = fromIntegral day
-                                               }
-                       , DB.userBalance = DB.Stellarium 0
+    let user = DB.User { DB.userName      = createUserName
+                       , DB.userNickname  = createUserNickname
+                       , DB.userEmail     = createUserEmail
+                       , DB.userBirthdate = createUserBirthdate
+                       , DB.userDate      = DB.Date { DB.year  = fromIntegral year
+                                                    , DB.month = fromIntegral month
+                                                    , DB.day   = fromIntegral day
+                                                    }
+                       , DB.userBalance   = DB.Stellarium 0
                        }
     userId <- runDB $ insert user
 
@@ -43,30 +46,24 @@ getUserByIdR userId = do
     user <- runDB $ get404 userId
     sendStatusJSON ok200 $ toJSON user
 
-{-
 data UserPatch = UserPatch
     { patchUserName  :: Maybe Text
-    , patchUserUserName :: Maybe Text
+    , patchUserNickname :: Maybe Text
+    , patchUserEmail :: Maybe Text
     } deriving (Generic, FromJSON)
 
 
-patchUserByIdR :: UserId -> Handler Value
+patchUserByIdR :: DB.UserId -> Handler Value
 patchUserByIdR userId = do
     UserPatch {..} <- requireCheckJsonBody :: Handler UserPatch
 
-    patchData <- return [
-            column =. fromJust maybeValue
-            | (column, maybeValue) <- [
-                (UserName, userMaybeName),
-                (UserEmail, userMaybeEmail)
-            ]
-            , isJust maybeValue
-        ]
-
+    let patchData = catMaybes [ (DB.UserName     =.) <$> patchUserName
+                              , (DB.UserNickname =.) <$> patchUserNickname
+                              , (DB.UserEmail    =.) <$> patchUserEmail
+                              ]
     runDB $ update userId patchData
 
     sendStatusJSON noContent204 emptyObject
--}
 
 deleteUserByIdR :: DB.UserId -> Handler Value
 deleteUserByIdR userId = do
